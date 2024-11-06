728a02a0f63723784c72e3d6858b59a19c3f4b8f
Foster-11 <rt.susana@hotmail.com>
2024-11-06 14:51:24 -0500

    Se añadió metodo para registrar un prestamo en el controlador y un post en los routes para prestamos

--- src/routes/bancoestebanquito.routes.js
+++ src/routes/bancoestebanquito.routes.js
@@ -5,19 +5,21 @@ import { metodosPrestamos } from "../controllers/prestamo.controllers.js";
 import { metodosReportes } from "../controllers/reporte.controllers.js";
 import { metodosRegister } from "../controllers/register.controllers.js";
 import { metodosTransferencia } from "../controllers/transferencia.controllers.js";
-import cors from 'cors'
+import cors from 'cors';
 const router = Router();
 
 // Routes
 
-router.get("/users",metodosUsuarios.getUsuarios);
-router.post("/user", cors({origin: "http://localhost:5173"}), metodosUsuarios.getUsuario);
-router.get("/transfers",metodosTransaccion.getTransacciones);
-router.post("/transfer",cors({origin: "http://localhost:5173"}),metodosTransaccion.getTransaccion);
-router.get("/loans",metodosPrestamos.getPrestamos);
-router.get("/loan",metodosPrestamos.getPrestamo);
-router.get("/reports",metodosReportes.getReportes);
-router.get("/report",metodosReportes.getReporte);
-router.post("/register",cors({origin: "http://localhost:5173"}),metodosRegister.registerUser);
-router.post("/transferir",cors({origin: "http://localhost:5173"}),metodosTransferencia.registerTransferencia);
+router.get("/users", metodosUsuarios.getUsuarios);
+router.post("/user", cors({ origin: "http://localhost:5173" }), metodosUsuarios.getUsuario);
+router.get("/transfers", metodosTransaccion.getTransacciones);
+router.post("/transfer", cors({ origin: "http://localhost:5173" }), metodosTransaccion.getTransaccion);
+router.get("/loans", metodosPrestamos.getPrestamos);
+router.get("/loan", metodosPrestamos.getPrestamo);
+router.post("/makeloan", cors({ origin: "http://localhost:5173" }), metodosPrestamos.registerPrestamo);
+router.get("/reports", metodosReportes.getReportes);
+router.get("/report", metodosReportes.getReporte);
+router.post("/register", cors({ origin: "http://localhost:5173" }), metodosRegister.registerUser);
+router.post("/transferir", cors({ origin: "http://localhost:5173" }), metodosTransferencia.registerTransferencia);
+
 export default router;
